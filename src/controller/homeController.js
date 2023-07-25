59c8c6c29f6925175d893e363438bff7dc4c9c31
nguyen dang <nguyendang1547@gmail.com>
2023-07-25 19:27:05 +0700

    Router parameter

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,23 @@
+import pool from '../configs/connectDB';
 
 
-let getHomePage = (req, res) => {
-    return res.render('index.ejs');
+
+let getHomePage = async (req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM `user`');
+
+    return res.render('index.ejs', {data: rows})
+}
+
+let getDetailPage = async (req, res) => {
+    console.log(req.params)
+    const [user, ] = await pool.execute(`SELECT * FROM user WHERE id = ${req.params.userId}`)
+    return res.send(user)
 }
 
 
 module.exports = {
-    getHomePage
+    getHomePage,
+    getDetailPage
 }
 
 
--- src/route/web.js
+++ src/route/web.js
@@ -5,12 +5,11 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
+    router.get('/detail/user/:userId', homeController.getDetailPage)
 
-    router.get('/about', (req, res) => {
-        res.send("Im Dang");
-    })
+   
 
-    return app.use('', router)
+    return app.use('/', router)
 }
 
 
